0024f98a7fbbfe3e182e3e1eea1f13d91d5c1b25
lyhtll <a62346986@gmail.com>
2025-11-13 20:48:25 +0900

    delete impl

--- src/main/kotlin/com/example/auth/domain/user/service/UserService.kt
+++ src/main/kotlin/com/example/auth/domain/user/service/UserService.kt
@@ -1,8 +1,14 @@
 package com.example.auth.domain.user.service
 
-import com.example.auth.domain.user.domain.User
 import com.example.auth.domain.user.dto.response.GetMeResponse
+import com.example.auth.global.security.util.SecurityUtil
+import org.springframework.stereotype.Service
 
-interface UserService {
-    fun getMe(): GetMeResponse
+@Service
+class UserService(
+    private val securityUtil: SecurityUtil
+) {
+    fun getMe(): GetMeResponse {
+        return GetMeResponse.Companion.of(securityUtil.getCurrentUser())
+    }
 }
