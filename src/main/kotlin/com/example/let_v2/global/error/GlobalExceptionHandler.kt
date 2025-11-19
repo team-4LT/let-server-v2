ef00af55934e1b9db1ec1ed72e900c130e7ba3c2
lyhtll <a62346986@gmail.com>
2025-11-19 09:45:21 +0900

    feat :: handleValidationException

--- src/main/kotlin/com/example/let_v2/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/let_v2/global/error/GlobalExceptionHandler.kt
@@ -1,6 +1,7 @@
 package com.example.let_v2.global.error
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -10,6 +11,16 @@ class GlobalExceptionHandler {
     @ExceptionHandler(CustomException::class)
     fun handleCustomException(ex: CustomException) =
         ErrorResponse.from(ex.error)
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationException(ex: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        val errorMessage = ex.bindingResult.fieldErrors
+            .firstOrNull()?.defaultMessage ?: "입력값이 올바르지 않습니다"
+
+        return ResponseEntity
+            .status(400)
+            .body(ErrorResponse(400, errorMessage))
+    }
 
     @ExceptionHandler(Exception::class)
     fun handleAll(ex: Exception) =
