82bd53d7a1340c4290979fd5d6ac28cc64fb586a
lyhtll <a62346986@gmail.com>
2025-12-03 20:02:15 +0900

    feat: Eater 에러 타입 정의
    - INVALID_GRADE 에러 추가 (학년 유효성 검증용)

--- src/main/kotlin/com/example/let_v2/domain/eater/error/EaterError.kt
+++ src/main/kotlin/com/example/let_v2/domain/eater/error/EaterError.kt
@@ -1,4 +1,11 @@
 package com.example.let_v2.domain.eater.error
 
-enum class EaterError {
+import com.example.let_v2.global.error.CustomError
+import org.springframework.http.HttpStatus
+
+enum class EaterError(
+    override val message: String,
+    override val status: Int
+): CustomError {
+    INVALID_GRADE("Invalid grade value. Grade must be 1~3.", HttpStatus.BAD_REQUEST.value())
 }
