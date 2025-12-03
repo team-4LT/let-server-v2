da075ce235891af8432eb74a7a2341a08e4874ba
lyhtll <a62346986@gmail.com>
2025-12-03 20:02:07 +0900

    feat: UserEaterResponse DTO 구현
    - 식사자 조회용 User 응답 DTO 생성
    - userId, studentId, realName 필드 포함

--- src/main/kotlin/com/example/let_v2/domain/user/dto/response/UserEaterResponse.kt
+++ src/main/kotlin/com/example/let_v2/domain/user/dto/response/UserEaterResponse.kt
@@ -1,3 +1,19 @@
 package com.example.let_v2.domain.user.dto.response
 
-data class UserEaterResponse()
+import com.example.let_v2.domain.user.domain.User
+
+data class UserEaterResponse(
+    val userId : Long,
+    val studentId : Int,
+    val realName : String
+){
+    companion object{
+        fun of(user : User): UserEaterResponse {
+            return UserEaterResponse(
+                userId = user.id!!,
+                studentId = user.studentId,
+                realName = user.name
+            )
+        }
+    }
+}
