9829ba0f193ddaf7a1c5c0da7e9fe96af839bc18
lyhtll <a62346986@gmail.com>
2025-12-03 20:02:20 +0900

    feat: EaterResponse DTO 구현
    - eaterId, user, eaten 필드 포함
    - of 팩토리 메서드 추가

--- src/main/kotlin/com/example/let_v2/domain/eater/dto/response/EaterResponse.kt
+++ src/main/kotlin/com/example/let_v2/domain/eater/dto/response/EaterResponse.kt
@@ -1,3 +1,20 @@
 package com.example.let_v2.domain.eater.dto.response
 
-data class EaterResponse()
+import com.example.let_v2.domain.eater.domain.Eater
+import com.example.let_v2.domain.user.dto.response.UserEaterResponse
+
+data class EaterResponse(
+    val eaterId : Long,
+    val user : UserEaterResponse,
+    val eaten : Boolean
+){
+    companion object {
+        fun of(eater: Eater, user: UserEaterResponse): EaterResponse {
+            return EaterResponse(
+                eaterId = eater.id!!,
+                user = user,
+                eaten = eater.eaten
+            )
+        }
+    }
+}
